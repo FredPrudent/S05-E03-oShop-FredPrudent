20855379c55ce6371e1d39b4b99d3f8f1c593715
jpruliere <jean@pruliere.fr>
2019-07-05 11:07:25 +0200

    home ok

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,5 +1,16 @@
 <?php
 
 class MainController {
-    
+    // notre première action : la homepage
+    public function home() {
+        $this->show('home');
+    }
+
+    // cette méthode se charge d'afficher les vues
+    // et donc de constituer les différentes pages de notre site
+    public function show($viewName) {
+        require_once __DIR__."/../views/header.tpl.php";
+        require_once __DIR__."/../views/$viewName.tpl.php";
+        require_once __DIR__."/../views/footer.tpl.php";
+    }
 }
--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo "ici, le frontcontroller";
+// étape incontournable si je veux instancier mon controller
+// il faut bien que PHP soit au courant de l'existence de la classe
+require_once __DIR__."/../app/controllers/MainController.php";
+
+// on instancie le controller
+$controller = new MainController();
+
+// on récupère l'url demandée par le visiteur à la base
+$route = isset($_GET['_url']) ? $_GET['_url'] : '/';
+
+switch ($route) {
+    case '/':
+        $controller->home();
+        break;
+}
